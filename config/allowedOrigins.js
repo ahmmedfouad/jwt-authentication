4dc45632c2ccc384b98004b42321542c7058689a
ahmmed fouad <ahmmedfouad2000@gmail.com>
2024-08-30 08:55:32 +0300

    fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 // allowedOrigins.js
 const allowedOrigins = [
-    "https://jwt-authentication-orpin.vercel.app/",
-    "https://jwt-authentication-o6esadyth-ahmed-fouads-projects-0d10054a.vercel.app",
-    "https://jwt-authentication-git-main-ahmed-fouads-projects-0d10054a.vercel.app"
+    "jwt-authentication-orpin.vercel.app/",
+    "jwt-authentication-git-main-ahmed-fouads-projects-0d10054a.vercel.app",
+    "jwt-authentication-i4k7dwz33-ahmed-fouads-projects-0d10054a.vercel.app"
     
 ];
 
