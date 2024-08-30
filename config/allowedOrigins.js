3896be86863ee0b382cb0544cca8d45071d41864
ahmmed fouad <ahmmedfouad2000@gmail.com>
2024-08-30 10:04:59 +0300

    fix outside calling

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
-// allowedOrigins.js
+
 const allowedOrigins = [
-    
+    'http://localhost:5000', // Your local development domain
+    'https://jwt-authentication-orpin.vercel.app/', // Your production domain
 ];
 
 module.exports = allowedOrigins;
