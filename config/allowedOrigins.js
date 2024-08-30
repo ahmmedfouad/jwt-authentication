7ddc43fa4a3b8bb67a73dfeac50934ab7ccdeffe
ahmmed fouad <ahmmedfouad2000@gmail.com>
2024-08-30 08:46:42 +0300

    add /

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     "https://jwt-authentication-orpin.vercel.app/",
     "https://jwt-authentication-o6esadyth-ahmed-fouads-projects-0d10054a.vercel.app",
     "https://jwt-authentication-git-main-ahmed-fouads-projects-0d10054a.vercel.app"
-   
+    
 ];
 
 module.exports = allowedOrigins;
